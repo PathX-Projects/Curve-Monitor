1fe406de6ef1b0863c0df7a54e153d10906f8cbc
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2022-11-05 01:01:04 -0500

    added cli tool

--- cli.sh
+++
@@ -1,2 +0,0 @@
-echo 'Running CLI tool for the Curve monitor'
-python -c 'from curve_monitor.cli_tool import CurveMonitorCLI;CurveMonitorCLI().run()'
---
+++ unix_cli.sh
@@ -0,0 +1,2 @@
+echo 'Running CLI tool for the Curve monitor'
+python3 -c 'from curve_monitor.cli_tool import CurveMonitorCLI;CurveMonitorCLI().run()'
