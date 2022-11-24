771ebc9392b0db718a2eabc64a0b5bf70654547e
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2022-11-24 01:08:51 -0600

    upgrades and bugfixes

--- curve_monitor/_config.py
+++ curve_monitor/_config.py
@@ -8,7 +8,7 @@ GETPOOLS_BASE_ENDPOINT = "https://api.curve.fi/api/getPools/{network_id}/{tag_id
 CURVE_POOL_BASEURL = "https://curve.fi/#/{network_id}/pools/{pool_id}"
 
 # Process:
-POLLING_PERIOD = 10  # Poll pools every 10 seconds
+POLLING_PERIOD = 15  # Poll pools every 10 seconds
 
 # TELEGRAM SETUP:
 TELEGRAM_CHAT_IDS = [-623989512]  # Telegram channels that will receive alerts
